93b06e1b1b444bea11cada3203f8df47a8145bfa
ramanujdas <ramanujds9@gmail.com>
2021-02-04 12:27:47 +0530

    Spring boot

---
+++ spring-boot-simple-app/src/main/java/com/example/demo/SpringBootSimpleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleAppApplication.class, args);
+	}
+
+}
